3d10df70e0785046e55fa07ac994d8ca52edce61
Tobias Kästle <t.kaestle@vivaconagua.org>
2025-06-12 11:20:14 +0200

    reset hooks

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,18 +2,18 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      boxShadow: {
-        bottom: '0px 10px 10px -5px rgba(0, 0, 0, 0.25)',
-        right: '10px 0px 10px -5px rgba(0, 0, 0, 0.15)',
-      },
-      zIndex: {
-        100: '100',
-        200: '200',
-      },
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            boxShadow: {
+                bottom: '0px 10px 10px -5px rgba(0, 0, 0, 0.25)',
+                right: '10px 0px 10px -5px rgba(0, 0, 0, 0.15)',
+            },
+            zIndex: {
+                100: '100',
+                200: '200',
+            },
+        },
     },
-  },
 };
